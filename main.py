1e2c185da4a8720be47e8a2713aacb9eb09641a1
geraldineaynaya <geraldinaynayallaja@gmail.com>
2024-11-27 14:49:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-Print( "My experience with Python" )
+print( "My experience with Python" )
 #Añadir presentación 
-Print( "Estaba muy intigrada por python, ya que no tenía experiencia previa en ningún nivel de programación. Esto resulto en un desafío total, ya que todo lo que me enseñaban era nuevo para mi")
+print( "Estaba muy intigrada por python, ya que no tenía experiencia previa en ningún nivel de programación. Esto resulto en un desafío total, ya que todo lo que me enseñaban era nuevo para mi")
 from PIL import Image 
 
 imagen = Image.open ("Fotopython.jpg")
 imagen.show()
-Print ("Era clave practicar constantemente para no olvidarme de los conceptos aprendidos en clase, asimismo, me sirvió para darme cuenta de la infinidad de usos que python tiene y lo creativo que puede ser")
+print ("Era clave practicar constantemente para no olvidarme de los conceptos aprendidos en clase, asimismo, me sirvió para darme cuenta de la infinidad de usos que python tiene y lo creativo que puede ser")
